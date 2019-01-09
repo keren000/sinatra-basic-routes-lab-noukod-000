658f4d19c20665c8d61811f8b7125d00c91df43b
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-09 19:36:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Kéren-Happuc."
+  end
+
+  get '/hometown' do
+    "My hometown is 3 rue Nelson 2 Carrefour."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Perfect!."
+  end
 end
